2bd238d7f3881ade97a51c9310d99a9f89b9e785
Samuel Martinez <samuel.martinez192@gmail.com>
2019-05-30 03:00:06 -0600

    Agregando pestanañas nuevas

--- PrroyectoUAMI/Controllers/HomeController.cs
+++ PrroyectoUAMI/Controllers/HomeController.cs
@@ -16,48 +16,6 @@ namespace PrroyectoUAMI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Users()
-        {
-            IEnumerable<User> usuarios = null;
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:52664/api/");
-
-                //Called Member default GET All records  
-                //GetAsync to send a GET request   
-                // PutAsync to send a PUT request  
-                var responseTask = client.GetAsync("user");
-                responseTask.Wait();
-
-                //To store result of web api response.   
-                var result = responseTask.Result;
-
-                //If success received   
-                if (result.IsSuccessStatusCode)
-                {
-                    var readTask = result.Content.ReadAsAsync<IList<User>>();
-                    readTask.Wait();
-
-                    usuarios = readTask.Result;
-                }
-                else
-                {
-                    //Error response received   
-                    usuarios = Enumerable.Empty<User>();
-                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
-                }
-            }
-
-            return View(usuarios);
-        }
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
